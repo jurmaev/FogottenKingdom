f797b8fdab643198fc56c66144811c8270a9f1fe
jurmaev <smartemii03@gmail.com>
2022-10-23 14:14:26 +0500

    добавил противника мишень
    При попадании в мишень, она ненадолго мигает красным цветом.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private Collider2D col;
+
+    private int delay = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(WaitAndDestroy());
+        col = GetComponent<Collider2D>();
+        Physics2D.IgnoreCollision(FindObjectOfType<Player>().GetComponent<Collider2D>(), col);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+    
+    IEnumerator WaitAndDestroy(){
+        yield return new WaitForSeconds(delay);
+        Destroy (gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
